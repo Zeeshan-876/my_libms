348b36381c3cffc5cb62d952d5a828ebfc583a9a
Zeeshan-876 <85904241+Zeeshan-876@users.noreply.github.com>
2021-07-31 04:16:29 -0700

    Add files via upload

---
+++ libms-main/database/migrations/2021_07_31_224925_create_setup_book_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSetupBookType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('setup_book_types', function (Blueprint $table) {
+            //
+            $table->bigIncrements('book_status_id');
+            $table->string('book_status_title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('setup_book_types', function (Blueprint $table) {
+            //
+        });
+    }
+}
